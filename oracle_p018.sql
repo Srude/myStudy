05ab51cf04c3e0ac33bd0c622ebad783dc85bf95
Srude <s22030@gsm.hs.kr>
2022-11-24 17:24:35 +0900

    CASE WHEN

---
+++ oracle_p018.sql
@@ -0,0 +1,8 @@
+SELECT EMPNO, ENAME, JOB, SAL,
+       CASE JOB
+           WHEN 'MANAGER' THEN SAL*1.1
+           WHEN 'SALEMAN' THEN SAL*1.05
+           WHEN 'ANALYST' THEN SAL
+           ELSE SAL*1.03
+           END AS UPSAL
+FROM EMP
